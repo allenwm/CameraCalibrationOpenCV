2331e625de6a1eb9a214faf911f14852d2a9b0e7
allenwm <506763070@qq.com>
2016-09-12 17:06:45 +0800

    new draw chessboard corners ： vector<Point2f>

--- OpenCVFind/OpenCVFind/OpenCVFind.cpp
+++ OpenCVFind/OpenCVFind/OpenCVFind.cpp
@@ -1,6 +1,7 @@
 #include <opencv2\opencv.hpp>
 #include <vector>
 #include <stdio.h>
+#include <string.h>
 
 using namespace cv;
 using namespace std;
@@ -10,18 +11,20 @@ int g_boardwidth = 6;
 int g_boardheight = 7;
 int g_boardtotal;
 Size g_boardSize;
-Mat g_image_points;
-Mat g_grayImage, g_srcImage, g_cvmaImage;
-char * g_pinName;
+vector<vector<Point2f>> g_imagePoints;			//角点坐标
+vector<vector<Point3f>> g_objectPoints;			//世界坐标
 
-void initFindCorner()
+Mat camerMatirx;								//相机内参矩阵
+Mat disCoeffs;									//畸变矩阵
+
+Mat g_grayImage, g_srcImage, g_cvmaImage;
+
+void initFindCorner(char * fileName)
 {
 	g_boardSize = Size(g_boardwidth, g_boardheight);
 	g_boardtotal = g_boardheight * g_boardwidth;
-
-	g_image_points = Mat::zeros(Size(2, g_boardtotal), CV_8U);
 	
-	g_srcImage = imread(g_pinName);
+	g_srcImage = imread(fileName);
 	imshow("【源图像】", g_srcImage);
 
 	cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
@@ -30,37 +33,52 @@ void initFindCorner()
 
 void findCornerWork()
 {
-	g_found = findChessboardCorners(g_grayImage, g_boardSize, g_image_points, 3);
+	vector<Point2f> CornerConers;
+	vector<Point3f> objectConers;
+
+	g_found = findChessboardCorners(g_grayImage, g_boardSize, CornerConers, 3);
 	
-	cornerSubPix(g_grayImage, g_image_points, Size(12, 12), Size(-1, -1), TermCriteria(
+	cornerSubPix(g_grayImage, CornerConers, Size(12, 12), Size(-1, -1), TermCriteria(
 		CV_TERMCRIT_EPS + CV_TERMCRIT_ITER,	40,	0.001));
+	
+	//输入角点的世界坐标值
+	for (int i = 0; i < g_boardheight; i++)
+	{
+		for (int j = 0; j < g_boardwidth; j++)
+		{
+			objectConers.push_back(Point3f(i, j, 0.0f));
+		}
+	}
 
 	for (int i = 0; i < g_boardtotal; i++)
 	{
-		uchar * data = g_image_points.ptr<uchar>(i);
-		for (int j = 0; j < 2; j++)
-		{
-			cout << (int)data[j] << "   ";
-		}
-		cout << endl;
+		cout << CornerConers[i].x << "   " << CornerConers[i].y << endl;
 	}
 
-	drawChessboardCorners(g_cvmaImage, g_boardSize, g_image_points, g_found);
+	drawChessboardCorners(g_cvmaImage, g_boardSize, CornerConers, g_found);
 	imshow("【角点标识图】", g_cvmaImage);
 }
 
-int main(int argc, char * argv[])
+void calcuCornerCoor(char * prefix, char * postfix, int ImageN)
 {
-	g_pinName = new char[128];
-
+	char filename[200] = {0};
 	int i;
-	for ( i = 1; i < 15; i++)
+	for ( i = 1; i < ImageN; i++)
 	{
-		sprintf(g_pinName, "E:\\openCV document\\OpenCV demarcate\\Image\\%d.jpg", i);
-		initFindCorner();
+		sprintf(filename, "%s%d%s", prefix, i, postfix);
+
+		initFindCorner(filename);
 		findCornerWork();
 		waitKey(10);
 	}
+}
+int main(int argc, char * argv[])
+{
+	char prefixName[100] = { "E:\\openCV document\\OpenCV demarcate\\Image\\" };
+	char postfixName[10] = { ".jpg" };
+	int ImageN = 15;
+	calcuCornerCoor(prefixName, postfixName, ImageN);
+
 	return 0;
 }
 
